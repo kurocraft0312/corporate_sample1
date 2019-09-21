33d002e59c17e834379c95474700091fa132ca7a
kurocraft0312 <kurocraft0312@gmail.com>
2019-09-22 00:47:16 +0900

    スムーススクロールの準備＆jQueryを実装しました。

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,18 @@
 'use strict';
 
+$(function(){
+    // #で始まるアンカーをクリックしたとき
+    $('a[href^="#"]').click(function() {
+        //スクロール速度(ミリ秒)
+        var speed = 400;
+        //アンカーの値取得
+        var href = $(this).attr("href");
+        //移動先を取得
+        var target = $(href === "#" || href === "" ? 'html' : href);
+        //移動先を数値で取得
+        var position = target.offset().top;
+        //スムーススクロール
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+    });
+});
